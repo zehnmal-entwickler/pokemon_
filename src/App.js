cc7b1f41c1e265ef8b4c612a9fcad1e185968364
David Kai Landes <davidkailandes@gmail.com>
2023-08-29 22:12:20 -0400

    installed Axios to fect API, added some minor logic to display names of pokemons, added some files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PokeList from "./PokeList";
+import axios from "axios";
+import "./app.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemon(res.data.results.map((poke) => poke.name));
+    });
+  }, []);
+
   return (
-    <div>
-      NOTHING HERE FOR NOW
+    <div className="app_backgrnd">
+      <PokeList pokemon={pokemon} />
     </div>
   );
 }
